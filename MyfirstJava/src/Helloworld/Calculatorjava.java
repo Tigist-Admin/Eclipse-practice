dea827f3b9c3521860cbe68000da24b9c5e782c7
Tigist-Admin <gebretensayt@gmail.com>
2021-04-17 17:59:22 -0400

    commit my first source code

---
+++ MyfirstJava/src/Helloworld/Calculatorjava.java
@@ -0,0 +1,11 @@
+package Helloworld;
+
+public class Calculatorjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+	}
+
+}
