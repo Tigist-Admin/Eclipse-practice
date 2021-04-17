bf95ae0742b6679bb62d6816f6cdc042bf024a37
Tigist-Admin <gebretensayt@gmail.com>
2021-04-17 18:28:53 -0400

    commit employee info

---
+++ MyfirstJava/src/secondpractice/Helloagain.java
@@ -0,0 +1,18 @@
+package secondpractice;
+
+public class Helloagain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Janet Jackson";
+		double salary = 2000;
+		double bonus = 100;
+		
+		System.out.println("Name of the Employee: " + name);
+		System.out.println("Salary of the Employee: " + salary);
+		System.out.println("Bonus of the Employee: " + bonus);
+		
+	}
+
+}
